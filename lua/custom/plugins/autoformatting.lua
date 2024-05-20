987742f37788752a22e64cc9be92e89ef04b4b08
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-05-20 14:39:55 +0200

    Included codespell formatter.
    Added codespell formatter.

--- lua/custom/plugins/autoformatting.lua
+++ lua/custom/plugins/autoformatting.lua
@@ -12,7 +12,7 @@ return { -- Autoformat
     },
   },
   opts = {
-    notify_on_error = false,
+    notify_on_error = true,
     format_on_save = function(bufnr)
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
@@ -25,6 +25,7 @@ return { -- Autoformat
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
+      -- ['*'] = { 'codespell' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
