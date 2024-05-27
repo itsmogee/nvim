a54d0add96e32b796370c80785868b32b5251cfc
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-05-28 00:47:45 +0200

    Updated the filter to remove annoying messages.
    After an undo annoying messages popped up.
    Enabled inc_rename using noice.

--- lua/custom/plugins/noice-ui.lua
+++ lua/custom/plugins/noice-ui.lua
@@ -18,6 +18,17 @@ return -- lazy.nvim
         },
         opts = { skip = true },
       },
+      {
+        filter = {
+          event = 'msg_show',
+          any = {
+            { find = '%d+L, %d+B' },
+            { find = '; after #%d+' },
+            { find = '; before #%d+' },
+          },
+        },
+        view = 'mini',
+      },
     },
     lsp = {
       -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
@@ -38,7 +49,7 @@ return -- lazy.nvim
       bottom_search = false, -- use a classic bottom cmdline for search
       command_palette = true, -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
-      inc_rename = false, -- enables an input dialog for inc-rename.nvim
+      inc_rename = true, -- enables an input dialog for inc-rename.nvim
       lsp_doc_border = true, -- add a border to hover docs and signature help
     },
   },
