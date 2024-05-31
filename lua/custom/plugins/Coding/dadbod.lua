f00e11b8ea5919667d466e427986667a178163d2
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-05-31 14:23:48 +0200

    Update : Dadbod keymaps and config
    DBUI no longer shows help hint.
    Added Keymaps for DBUI.

--- lua/custom/plugins/Coding/dadbod.lua
+++ lua/custom/plugins/Coding/dadbod.lua
@@ -24,9 +24,21 @@ return {
     },
     init = function()
       -- your dbui configuration
+      vim.g.db_ui_show_help = 0
       vim.g.db_ui_use_nerd_fonts = 1
       vim.g.db_ui_show_database_icon = 1
       vim.g.db_ui_use_nvim_notify = 1
+
+      require('which-key').register {
+        ['<leader>D'] = {
+          name = '󰆼 Db Tools',
+          s = { ':DBUI<cr>', ' DB UI' },
+          u = { ':DBUIToggle<cr>', ' DB UI Toggle' },
+          f = { '<cmd>DBUIFindBuffer<cr>', ' DB UI Find buffer' },
+          r = { '<cmd>DBUIRenameBuffer<cr>', ' DB UI Rename buffer' },
+          l = { '<cmd>DBUILastQueryInfo<cr>', ' DB UI Last query infos' },
+        },
+      }
     end,
   },
 }
