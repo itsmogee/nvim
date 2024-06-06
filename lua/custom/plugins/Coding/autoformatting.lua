6de30447d8c5aa02db97a60a87bd3ebc5f90c5a8
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-06-06 13:01:23 +0200

    Fix : added stylue formatter back to autoformatting

--- lua/custom/plugins/Coding/autoformatting.lua
+++ lua/custom/plugins/Coding/autoformatting.lua
@@ -24,6 +24,7 @@ return { -- Autoformat
       }
     end,
     formatters_by_ft = {
+      lua = { 'stylua' }
       -- ['*'] = { 'codespell' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
