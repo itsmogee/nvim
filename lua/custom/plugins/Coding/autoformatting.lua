9aacf6bf7d63e6e29ac6a3e70f95a3d6ae91488d
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-06-06 18:56:11 +0200

    Add : sql_formatter
    added the sql_formatter and configured it to use a json config file

--- lua/custom/plugins/Coding/autoformatting.lua
+++ lua/custom/plugins/Coding/autoformatting.lua
@@ -24,6 +24,8 @@ return { -- Autoformat
       }
     end,
     formatters_by_ft = {
+      sql = { 'sql_formatter' },
+      mysql = { 'sql_formatter' },
       -- ['*'] = { 'codespell' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
@@ -31,6 +33,26 @@ return { -- Autoformat
       -- You can use a sub-list to tell conform to run *until* a formatter
       -- is found.
       -- javascript = { { "prettierd", "prettier" } },
+    },
+    formatters = {
+      sql_formatter = {
+        -- This can be a string or a function that returns a string.
+        -- When defining a new formatter, this is the only field that is required
+        -- A list of strings, or a function that returns a list of strings
+        -- Return a single string instead of a list to run the command in a shell
+        inherit = false,
+        command = 'sql-formatter',
+        args = {
+          '-l',
+          'mysql',
+          '--config',
+          vim.fn.expand '~/.config/nvim/sqlformatter.json',
+        },
+        -- Send file contents to stdin, read new contents from stdout (default true)
+        -- When false, will create a temp file (will appear in "$FILENAME" args). The temp
+        -- file is assumed to be modified in-place by the format command.
+        -- stdin = true,
+      },
     },
     vim.keymap.set({ 'n', 'v' }, ',fc', function()
       require('conform').format { async = true, lsp_fallback = false }
