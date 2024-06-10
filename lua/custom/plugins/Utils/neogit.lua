4275d536149a921471066e5cdd939ea315911613
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-06-10 02:25:25 +0200

    Update : added keymaps for neogit and changed default icons.

--- lua/custom/plugins/Utils/neogit.lua
+++ lua/custom/plugins/Utils/neogit.lua
@@ -5,5 +5,36 @@ return {
     'sindrets/diffview.nvim',
     'nvim-telescope/telescope.nvim',
   },
-  config = true
+  cmd = 'Neogit',
+  keys = {
+    {
+      '<leader>go',
+      ':Neogit <CR>',
+      desc = "Neogit [O]pen",
+    },
+    {
+      '<leader>gP',
+      ':Neogit push<CR>',
+      desc = "Neogit [P]ush",
+    },
+    {
+      '<leader>gp',
+      ':Neogit pull<CR>',
+      desc = "Neogit [P]ull",
+    },
+    {
+      '<leader>gb',
+      ':Telescope git_branches<CR>',
+      desc = "Git [B]ranches",
+    },
+  },
+  opts = {
+    signs = {
+      item = { "+", "-" },
+      section = { "", "" },
+    },
+    integrations = {
+      diffview = true,
+    },
+  }
 }
