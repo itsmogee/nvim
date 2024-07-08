d0fc65153ec0844fd4d191306f2f57f01cc1f999
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-08 22:09:00 +0200

    added : mason and mason-install plugins
    mason package manager for lsps/dap/linters and formatters

---
+++ lua/custom/plugins/Coding/mason.lua
@@ -0,0 +1,49 @@
+return { -- LSP Configuration & Plugins
+  {
+    'williamboman/mason.nvim',
+    dependencies = 'williamboman/mason-lspconfig.nvim',
+    config = function() -- NOTE: Must be loaded before dependants
+      -- import mason
+      local mason = require 'mason'
+
+      -- import mason-lspconfig
+      local mason_lsp
+
+      -- enable mason and configure icons
+      mason.setup {
+        ui = {
+          icons = {
+            package_installed = '✓',
+            package_pending = '➜',
+            package_uninstalled = '✗',
+          },
+        },
+      }
+    end,
+  },
+  {
+    -- Automatically install LSPs and related tools to stdpath for Neovim
+    'williamboman/mason-lspconfig.nvim',
+    config = function()
+      require('mason-lspconfig').setup {
+        ensure_installed = {
+          'lua_ls',
+          'biome',
+          'html',
+          'pyright',
+          'ltex',
+          'rust_analyzer',
+          'tailwindcss',
+          'tsserver',
+          'cssls',
+          'emmet_ls',
+          'marksman',
+          'sqls',
+        },
+
+        -- auto-install configured servers (with lspconfig)
+        automatic_installation = true, --not the same as ensure_installed
+      }
+    end,
+  },
+}
