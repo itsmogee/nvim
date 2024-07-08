18cb301066e6c2c6a9dae6e6dd701234661749d1
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-08 22:11:09 +0200

    added : bare plugin
    handles bare repo files much easier with lazygit

---
+++ lua/custom/plugins/Utils/bare.lua
@@ -0,0 +1,10 @@
+return {
+  "ejrichards/baredot.nvim",
+  opts = {
+    -- Change this path
+    git_dir = "$HOME/dotfiles",
+    git_work_tree = "$HOME",
+    -- Filename pattern to find that will disable baredot
+    disable_pattern = "%.git"
+  }
+}
