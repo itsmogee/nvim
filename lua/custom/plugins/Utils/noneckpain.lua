ee2c01f999f602c995bcafa587c129e8a0dd1b9a
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-09 23:48:23 +0200

    added : noneckpain plugin
    centers the coding environment on the screen for more eronomic way
    of coding

---
+++ lua/custom/plugins/Utils/noneckpain.lua
@@ -0,0 +1,13 @@
+return {
+  'shortcuts/no-neck-pain.nvim',
+  version = '*',
+  opts = {
+    width = 90,
+    autocmds = {
+      enableOnVimEnter = false,
+    },
+    mappings = {
+      enabled = true,
+    },
+  },
+}
