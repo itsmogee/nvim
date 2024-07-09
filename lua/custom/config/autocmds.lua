f912efe131ff0ffe6d91d852fccb00eea4dfd31e
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-09 17:11:10 +0200

    added : autocommand for closing panels
    autocommand group for closing panels with 'q'

--- lua/custom/config/autocmds.lua
+++ lua/custom/config/autocmds.lua
@@ -19,3 +19,26 @@ vim.api.nvim_create_autocmd('BufEnter', {
   end,
   desc = 'Disable New Line Comment',
 })
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup('personal-' .. name, { clear = true })
+end
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup 'close_with_q',
+  pattern = {
+    'help',
+    'lspinfo',
+    'notify',
+    'spectre_panel',
+    'startuptime',
+    'checkhealth',
+    'dbout',
+    'gitsigns.blame',
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = event.buf, silent = true })
+  end,
+})
