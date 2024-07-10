ab2c011878c123ae0ae8568e4fe82a9d6fe17c76
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-10 19:06:19 +0200

    added : new typescript lsp
    vslts language server to replace tsserver

--- lua/custom/plugins/Coding/lspconfig.lua
+++ lua/custom/plugins/Coding/lspconfig.lua
@@ -271,5 +271,7 @@ return {
     lspconfig['emmet_language_server'].setup {
       filetypes = { 'css', 'eruby', 'html', 'javascript', 'javascriptreact', 'less', 'sass', 'scss', 'pug', 'typescriptreact' },
     }
+
+    lspconfig['vtsls'].setup {}
   end,
 }
