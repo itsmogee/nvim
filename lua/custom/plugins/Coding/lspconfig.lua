8d9e235dfdba9886bb293a3d1314a024ffd50893
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-11 23:30:06 +0200

    added : lsp icons
    icons for :
    error
    warning
    info
    hint

--- lua/custom/plugins/Coding/lspconfig.lua
+++ lua/custom/plugins/Coding/lspconfig.lua
@@ -15,6 +15,10 @@ return {
     { 'folke/neodev.nvim', opts = {} },
   },
   config = function()
+    vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticSignError' })
+    vim.fn.sign_define('DiagnosticSignWarn', { text = ' ', texthl = 'DiagnosticSignWarn' })
+    vim.fn.sign_define('DiagnosticSignInfo', { text = ' ', texthl = 'DiagnosticSignInfo' })
+    vim.fn.sign_define('DiagnosticSignHint', { text = ' ', texthl = 'DiagnosticSignHint' })
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
       callback = function(event)
