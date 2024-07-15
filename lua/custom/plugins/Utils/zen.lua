f7ed8f4a1c3558d58840318bf1e71047447194a1
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-15 08:45:24 +0200

    updated : keymap for zenmode && autoformatter
    stylua autoformatter made changes
    updated the way I was setting the keymap so that which key icons are applied

--- lua/custom/plugins/Utils/zen.lua
+++ lua/custom/plugins/Utils/zen.lua
@@ -1,27 +1,24 @@
 return -- Lua
 {
   {
-    "folke/twilight.nvim",
+    'folke/twilight.nvim',
     opts = {
 
       dimming = {
         alpha = 0.25, -- amount of dimming
         -- we try to get the foreground from the highlight groups or fallback color
-        color = { "Normal", "#ffffff" },
-        term_bg = "#000000", -- if guibg=NONE, this will be used to calculate text color
-        inactive = true,     -- when true, other windows will be fully dimmed (unless they contain the same buffer)
+        color = { 'Normal', '#ffffff' },
+        term_bg = '#000000', -- if guibg=NONE, this will be used to calculate text color
+        inactive = true, -- when true, other windows will be fully dimmed (unless they contain the same buffer)
       },
-      context = 10,          -- amount of lines we will try to show around the current line
-      vim.keymap.set('n', '<leader>tw', ':Twilight<CR>')
-
-    }
+      context = 10, -- amount of lines we will try to show around the current line
+      vim.keymap.set('n', '<leader>tw', ':Twilight<CR>'),
+    },
   },
   {
     'folke/zen-mode.nvim',
     event = 'VeryLazy',
-    opts =
-    {
-      vim.keymap.set('n', '<leader>z', ':ZenMode<CR>', { desc = "Zen Mode" }),
+    opts = {
       window = {
         backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
         -- height and width can be:
@@ -29,12 +26,12 @@ return -- Lua
         -- * a percentage of the width / height of the editor when <= 1
         -- * a function that returns the width or the height
         width = 120, -- width of the Zen window
-        height = 1,  -- height of the Zen window
+        height = 1, -- height of the Zen window
         -- by default, no options are changed for the Zen window
         -- uncomment any of the options below, or add other vim.wo options you want to apply
         options = {
-          signcolumn = "no",      -- disable signcolumn
-          number = false,         -- disable number column
+          signcolumn = 'no', -- disable signcolumn
+          number = false, -- disable number column
           relativenumber = false, -- disable relative numbers
           -- cursorline = false, -- disable cursorline
           -- cursorcolumn = false, -- disable cursor column
@@ -47,16 +44,16 @@ return -- Lua
         -- comment the lines to not apply the options
         options = {
           enabled = true,
-          ruler = true,    -- disables the ruler text in the cmd line area
+          ruler = true, -- disables the ruler text in the cmd line area
           showcmd = false, -- disables the command in the last line of the screen
           -- you may turn on/off statusline in zen mode by setting 'laststatus'
           -- statusline will be shown only if 'laststatus' == 3
-          laststatus = 0,               -- turn off the statusline in zen mode
+          laststatus = 0, -- turn off the statusline in zen mode
         },
-        twilight = { enabled = true },  -- enable to start Twilight when zen mode opens
+        twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
         gitsigns = { enabled = false }, -- disables git signs
         incline = { enabled = false },
-        tmux = { enabled = true },      -- disables the tmux statusline
+        tmux = { enabled = true }, -- disables the tmux statusline
 
         -- this will change the font size on kitty when in zen mode
         -- to make this work, you need to set the following kitty options:
@@ -64,21 +61,22 @@ return -- Lua
         -- - listen_on unix:/tmp/kitty
         kitty = {
           enabled = false,
-          font = "+4", -- font size increment
+          font = '+4', -- font size increment
         },
       },
 
       -- callback where you can add custom code when the Zen window opens
       on_open = function(win)
-        require("ibl").update { enabled = false }
-        require("incline").disable()
+        require('ibl').update { enabled = false }
+        require('incline').disable()
       end,
       -- callback where you can add custom code when the Zen window closes
       on_close = function()
-        require("ibl").update { enabled = true }
-        require("incline").enable()
+        require('ibl').update { enabled = true }
+        require('incline').enable()
       end,
-      vim.api.nvim_set_hl(0, "ZenBg", { ctermbg = 0 })
+      vim.api.nvim_set_hl(0, 'ZenBg', { ctermbg = 0 }),
     },
+    keys = { { '<leader>z', '<cmd>ZenMode<cr>', desc = 'Zen Mode' } },
   },
 }
